d3f4df1a8e9e54df07e6e1ea5da33de43d82bb87
sajib37 <sajibhasan9811@gmail.com>
2024-03-28 13:38:20 +0600

    project setup completed

---
+++ src/Layout/Root.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Root = () => {
+    return (
+        <div>
+            <h1>This Main layout</h1>
+        </div>
+    );
+};
+
+export default Root;
---
+++ src/Shared/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from '../../Pages/Home/Home';
+
+const Footer = () => {
+    return (
+        <div>
+            <h1>Footer</h1>
+            <Home></Home>
+        </div>
+    );
+};
+
+export default Footer;
